41d32633e7f3b389d1381fff5a1819dadf3b2c14
Lucile Gentner <lucile.gentner@gmail.com>
2016-04-13 16:55:30 +0200

    Add random motivation phrase in index.js

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
+const motivations = require('motivations');
+const pickOne = require('pick-one');
+
 app.get("/", function(request, response){
-	response.send("Index page");
+	response.send(pickOne(motivations));
 });
 
 app.get("/about", function(request, response){
 	response.send("About page");
 });
-
 
 module.exports = app;
